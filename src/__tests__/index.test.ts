563b26d74dad4d7afb6f1f164819fad43f866d8c
John Fazioli <john.fazioli@namecheap.com>
2021-03-24 14:10:15 +0100

    wip

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,7 @@
-it("Works", () => {
-  expect(true).toBe(true);
+import { plural } from "../utils/strings";
+
+it("Plural", () => {
+  expect(plural(0, "dogs", "dog", "none")).toBe("none");
+  expect(plural(1, "dogs", "dog")).toBe("dog");
+  expect(plural(2, "dogs", "dog")).toBe("dogs");
 });
